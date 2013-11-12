98f6ca52824e545e6ae2df7cf74cfde651a6f6ba
leoliu pku <leoliu.pku@gmail.com@77a52a0d-5b5a-0024-7867-7f92eaad0685>
2013-11-12 15:49:21 +0000

    ?????????

--- input.go
+++ input.go
@@ -1,4 +1,21 @@
 package main
 
 type indexentry struct {
+	level     []indexentrykv
+	pagefmt   string
+	page      int
+	pagerange pagerange
+}
+
+type pagerange int
+
+const (
+	page_normal pagerange = iota
+	page_open   pagerange = iota
+	page_close  pagerange = iota
+)
+
+type indexentrykv struct {
+	sortkey string
+	text    string
 }
