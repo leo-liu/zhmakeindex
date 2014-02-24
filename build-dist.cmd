0e85ec94db5d2011658aca4156697e328d91a561
leoliu pku <leoliu.pku@77a52a0d-5b5a-0024-7867-7f92eaad0685>
2014-02-24 12:16:14 +0000

    zhmakeindex: 版本升至 1.0。 更新程序帮助信息。 完成文档索引。 可执行文件目录从 release 改为 bin。

--- build-dist.cmd
+++ build-dist.cmd
@@ -3,20 +3,20 @@ set FLAGS=-ldflags "-s -w"
 
 set GOOS=windows
 set GOARCH=386
-go build %FLAGS% -o release/windows_x86/zhmakeindex.exe
+go build %FLAGS% -o bin/windows_x86/zhmakeindex.exe
 set GOARCH=amd64
-go build %FLAGS% -o release/windows_x64/zhmakeindex.exe
+go build %FLAGS% -o bin/windows_x64/zhmakeindex.exe
 
 set GOOS=linux
 set GOARCH=386
-go build %FLAGS% -o release/linux_x86/zhmakeindex
+go build %FLAGS% -o bin/linux_x86/zhmakeindex
 set GOARCH=amd64
-go build %FLAGS% -o release/linux_x64/zhmakeindex
+go build %FLAGS% -o bin/linux_x64/zhmakeindex
 
 set GOOS=darwin
 set GOARCH=386
-go build %FLAGS% -o release/darwin_x86/zhmakeindex
+go build %FLAGS% -o bin/darwin_x86/zhmakeindex
 set GOARCH=amd64
-go build %FLAGS% -o release/darwin_x64/zhmakeindex
+go build %FLAGS% -o bin/darwin_x64/zhmakeindex
 
 endlocal
