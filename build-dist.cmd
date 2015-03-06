367e485b0633774cd19ac1a6e1d4ae52bf3bbcbd
Liu Haiyang <leoliu.pku@gmail.com>
2015-03-06 17:31:27 +0800

    ?????????????? VERSION ?????????????? VERSION ??????? ?? README ? MENIFEST ???????

--- build-dist.cmd
+++ build-dist.cmd
@@ -2,8 +2,13 @@ setlocal
 %~d0
 cd %~p0
 
-for /f "delims=" %%i in ('hg parent --template "{rev}({node|short})"') do set Revision=%%i
-set FLAGS=-ldflags "-s -w -X main.Revision %Revision%"
+if exist VERSION (
+  for /f "delims=" %%i in (VERSION) do set zhmVersion=%%i
+) else (
+  set zhmVersion=devel
+)
+for /f "delims=" %%i in ('hg parent --template "{rev}({node|short})"') do set zhmRevision=%%i
+set FLAGS=-ldflags "-X main.Version %zhmVersion% -X main.Revision %zhmRevision%"
 
 set GOOS=windows
 set GOARCH=386
---
+++ doc/make.cmd
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+%~d0
+cd %~p0
+
+echo ==========^> Updating version...
+echo \def\zhmVersion{%%> zhm-version.tex
+if exist ..\VERSION (
+    type ..\VERSION >> zhm-version.tex
+) else (
+    echo devel%%>> zhm-version.tex
+)
+echo }>> zhm-version.tex
+echo \def\zhmRevision{%%>> zhm-version.tex
+hg parent --template "{rev}({node|short})" >> zhm-version.tex
+echo }>> zhm-version.tex
+
+echo ==========^> Compiling document...
+xelatex -interaction=batchmode zhmakeindex.tex
+echo ==========^> Running BibTeX...
+bibtex zhmakeindex
+echo ==========^> Running zhmakeindex...
+zhmakeindex zhmakeindex
+echo ==========^> Recompiling document...
+xelatex -interaction=batchmode zhmakeindex.tex
+xelatex -interaction=batchmode zhmakeindex.tex
+
+endlocal
