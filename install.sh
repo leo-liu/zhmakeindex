fa8f93922ae9d28c204441200cb9d4a6a0932a87
Liu Haiyang <leoliu.pku@gmail.com>
2015-06-01 13:57:28 +0800

    Bash 安装脚本

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -e "VERSION" ]
+then
+	zhmVersion=`cat VERSION`
+else
+	zhmVersion=devel
+fi
+
+zhmRevision=`git log -1 --pretty=format:"%h(%ai"`
+zhmRevision="${zhmRevision:0:18})"
+
+go install -ldflags "-X main.Version $zhmVersion -X main.Revision $zhmRevision"
