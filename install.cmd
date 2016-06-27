2e2cf85750d4be1ad21df9879ea6f2a448c8ee6b
Liu Haiyang <leoliu.pku@gmail.com>
2016-06-27 19:58:28 +0800

    更新编译脚本中链接选项，以支持新版本 go 工具链

--- install.cmd
+++ install.cmd
@@ -9,7 +9,7 @@ if exist VERSION (
 )
 for /f "delims=" %%i in ('git log -1 --pretty^=format:"%%h(%%ai"') do set zhmRevision=%%i
 set zhmRevision=%zhmRevision:~0,18%)
-set FLAGS=-ldflags "-X main.Version %zhmVersion% -X main.Revision %zhmRevision%"
+set FLAGS=-ldflags "-X main.Version=%zhmVersion% -X main.Revision=%zhmRevision%"
 
 go install %FLAGS%
 
